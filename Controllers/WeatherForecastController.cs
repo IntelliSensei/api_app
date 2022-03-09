4f156d5cc9aa0793bb2f84e29d7dbc4f978e86cf
KP <ace_643@hotmail.com>
2022-03-09 10:41:35 +0100

    new weather

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace api_app.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Toofaan", "Paad"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Toofaan", "Paad", "Garam Masala"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
